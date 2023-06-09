970f490f8891c8f90882681cb712bbf06f1fd8d5
LucileVincent <lucile.vincent@student.junia.com>
2023-06-09 12:29:56 +0200

    handle timeout error

--- processing.py
+++ processing.py
@@ -1,3 +1,5 @@
+import time
+
 def tuple_to_sequence(lst, width, shift):
     """Convert [(strt, stp, val)] to sequence of val.
     
@@ -15,16 +17,26 @@ def tuple_to_sequence(lst, width, shift):
     frame_id = 0
     ind = 0
     
-    while (frame_id * shift) < (last_frame * shift) and ind < len(lst):
-        if lst[ind][1] >= (frame_id * shift) >= lst[ind][0]:
-            while (frame_id * shift) < lst[ind][1]:
-                seq.append(lst[ind][2])
-                frame_id += 1
-            ind += 1
-        else:
-            while (frame_id * shift) < lst[ind][0]:
-                seq.append(None)
-                frame_id += 1
-                if (frame_id * shift) >= (last_frame * shift):
-                    return seq
+    try:
+        start_time = time.time()
+        while (frame_id * shift) < (last_frame * shift) and ind < len(lst):
+            if lst[ind][1] >= (frame_id * shift) >= lst[ind][0]:
+                while (frame_id * shift) < lst[ind][1]:
+                    seq.append(lst[ind][2])
+                    frame_id += 1
+                ind += 1
+            else:
+                while (frame_id * shift) < lst[ind][0]:
+                    seq.append(None)
+                    frame_id += 1
+                    if (frame_id * shift) >= (last_frame * shift):
+                        return seq
+            elapsed_time = time.time() - start_time
+            if elapsed_time > 30:  
+                raise TimeoutError("Generating the sequence took too long.")
+    except TimeoutError as e:
+        print(f"Timeout Error: {e}")
+        seq = [None] * len(lst)
+        pass
+    #print("Sequence", seq)
     return seq
